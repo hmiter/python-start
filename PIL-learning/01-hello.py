fa711cfbf6318e0b68c31f0f2d2d509815b47542
huangmin <huangmin@mgtv.com>
2018-12-12 11:04:51 +0800

    添加注释！
    Change-Id: I881e26bef7322eba21a196415440c2226b592005

--- PIL-learning/01-hello.py
+++ PIL-learning/01-hello.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+# 需要安卓pillow第三方库：pip install pillow
 
 from PIL import Image
 
 try:
     im = Image.open("cat.jpg");
-    print im.format, im.size, im.mode
-    print im.info
+    print(im.format, im.size, im.mode)
+    print(im.info)
     im.show()
 except IOError:
     pass
