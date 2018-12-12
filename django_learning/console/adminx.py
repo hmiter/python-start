7fe346b8b22ba58d5020f977e88a54f6de36db79
huangmin <huangmin@mgtv.com>
2018-12-12 15:07:21 +0800

    修改标题
    Change-Id: I7eeea5fdcb54438f894c94a6a6b13de28c290eeb

--- django_learning/console/adminx.py
+++ django_learning/console/adminx.py
@@ -6,9 +6,9 @@ from xadmin import views
 
 class GlobalSetting(object):
     # 设置后台顶部标题   
-    site_title = '我是后台管理'
+    site_title = '管理控制台'
     # 设置后台底部标题   
-    site_footer = '我是底部信息'
+    site_footer = '2018 敏达软件 '
     # 设置菜单可以折叠
     menu_style = 'accordion'
 
